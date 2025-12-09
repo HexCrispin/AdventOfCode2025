2d94cd3757c8dfb476b7bcc3067a31f9b1c161c2
Hex <theinferos@gmail.com>
2025-12-09 11:11:30 +0000

    Day5 split by version

--- Utils/fileUtils.js
+++ Utils/fileUtils.js
@@ -29,9 +29,20 @@ function readFileLinesFiltered(filePath) {
   return data.replace(/\r/g, '').split('\n').filter(line => line.trim() !== '' );
 }
 
+/**
+ * Reads a file and returns its contents as an array of lines, filtering out empty lines
+ * @param {string} filePath - Path to the file to read
+ * @returns {string[]} Array of non-empty lines from the file
+ */
+function readFileLinesFilteredEmpties(filePath) {
+  const data = fs.readFileSync(filePath, 'utf8');
+  return data.replace(/\r/g, '').split('\n');
+}
+
 module.exports = {
   readFile,
   readFileLines,
-  readFileLinesFiltered
+  readFileLinesFiltered,
+  readFileLinesFilteredEmpties
 };
 
